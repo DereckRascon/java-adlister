b614eee6bcc5dd2c8c703edb465acef0d400850c
Dereck Rascon <dereckrascon76@gmail.com>
2023-02-09 14:34:28 -0700

    finished JDBC exercise

---
+++ database/seeder.sql
@@ -0,0 +1,9 @@
+USE adlister_db;
+
+
+
+INSERT INTO users(username, email, password) VALUES ('dreck', 'dreck@gmail.com', 'fit');
+
+
+
+INSERT INTO ads(user_id, title, description) VALUES ('1', 'Power drill', 'Still works its just used');
